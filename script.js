fe2c3e80ff07b3f3cc0f9647c4306e85b67ea497
sasnar <juugotempin@yandex.ru>
2024-02-14 13:27:53 +0200

    Finished game logic, added both color modes for hover event, corrected grid creation.

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
 let sliderValue = document.querySelector('#slide'); 
 let sliderValueDisplay = document.querySelector('.sliderValue');
+let gameGrid = document.querySelector('.container');
+let blackMode = document.querySelector('#blackMode');
+let rgbMode = document.querySelector('#rgbMode');
+
 sliderValue.addEventListener('input', function () {
     sliderValueDisplay.textContent = `${sliderValue.value} x ${sliderValue.value}`;
+    createGrid();
 });
 
-let gameGrid = document.querySelector('.container');
-sliderValue.addEventListener('input', function () {
-    if (gameGrid.hasChildNodes) {
-        gameGrid.replaceChildren();
-    }
+blackMode.addEventListener('change', function () {
+    createGrid();
+});
+
+rgbMode.addEventListener('change', function () {
+    createGrid();
+});
+
+function getRandomColor () {
+    let red = Math.floor(Math.random() * 255 + 1);
+    let green = Math.floor(Math.random() * 255 + 1);
+    let blue = Math.floor(Math.random() * 255 + 1);
+    return rgb = `rgb(${red}, ${green}, ${blue})`
+}
+
+function createGrid() {
     let gridSize = sliderValue.value;
-    for (let i = 0; i < gridSize*gridSize; i++) {
+    let containerWidth = gameGrid.clientWidth; 
+    let cellWidth = containerWidth / gridSize; 
+    gameGrid.innerHTML = '';
+    for (let i = 0; i < gridSize * gridSize; i++) {
         let newCell = document.createElement('div');
         newCell.className = 'gridCell';
+        newCell.style.width = `${cellWidth}px`; 
+        newCell.style.height = `${cellWidth}px`; 
         gameGrid.appendChild(newCell);
-
     }
-    newCell.style.flex = "1 0 auto";
-});
+    let cells = document.querySelectorAll('.gridCell');
+    if (blackMode
+    .checked) {
+        cells.forEach(function (div) {
+            div.addEventListener('mouseover', function () {
+                div.style.backgroundColor = "black"; 
+            });
+        });
+    } else {
+        cells.forEach(function (div) {
+            div.addEventListener('mouseover', function () {
+                div.style.backgroundColor = getRandomColor();
+            });
+        });
+    } 
+}
+
+createGrid();
